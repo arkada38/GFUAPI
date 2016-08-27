7bd28c13a66d63bab3e5f96ad0807515d52f81d8
Arkadiy <arkada38@outlook.com>
2016-08-28 06:18:00 +0700

    0.1.2 - magrittr is deleted

--- R/Main.R
+++ R/Main.R
@@ -5,8 +5,6 @@
 # Rcpp::sourceCpp('src/cpp.cpp')
 
 getHistoricalQuote <- function(symbol, exchange, interval = 86400, period = "10Y") {
-  library(magrittr)
-
   url <- paste0("https://www.google.com/finance/getprices?q=",
                 symbol,"&x=",exchange,"&i=",interval,"&p=",period,"&f=d,c,v,k,o,h,l")
   url_content <- readLines(url)
@@ -14,8 +12,10 @@ getHistoricalQuote <- function(symbol, exchange, interval = 86400, period = "10Y
   if (length(url_content) > 7){
     df = read.table(text = parseString(url_content), sep = ",", colClasses = "numeric")
     names(df) <- c("Date", "Timezon_offset", "Close", "High", "Low", "Open", "Volume", "CDAYS")
+
     df <- data.frame(df$Date, df$Timezon_offset, df$Open, df$High, df$Low, df$Close, df$Volume)
     names(df) <- c("Date", "Timezon_offset", "OPEN", "HIGH", "LOW", "Close", "VOLUME")
+
     return(df)
   }
   else
