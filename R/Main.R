7bbe4345311e6f3e07815a34ed02f2b81c692b91
Arkadiy <arkada38@outlook.com>
2017-03-14 09:09:18 +0700

    VSA is updated

--- R/Main.R
+++ R/Main.R
@@ -5,8 +5,8 @@
 # d <- getHistoricalQuote('gbpusd', 'CURRENCY', interval = 900, period = '3d')
 # head(d, 15)
 
-getHistoricalQuote <- function(symbol, exchange, interval = 86400,
-  period = "10Y") {
+getHistoricalQuote <- function(symbol, exchange,
+                               interval = 86400, period = "10Y") {
   url <- paste0("https://www.google.com/finance/getprices?q=",
     toupper(symbol), "&x=", toupper(exchange), "&i=", interval,
     "&p=", period, "&f=d,c,v,k,o,h,l")
@@ -31,70 +31,120 @@ getHistoricalQuote <- function(symbol, exchange, interval = 86400,
   } else warning("Incorrect parameters")
 }
 
-getHistoricalVSAQuote <- function(data, period = 15, standardize = F) {
+getHistoricalVSAQuote <- function(data, period = 15) {
   # Height of bar
   data$Height <- data$High - data$Low
 
-  # Type of bar
-  for (i in 1:(nrow(data) - 1)) {
-    if (data$Close[i] > data$High[i + 1]) {
-      data$B[i] <- +2
-    } else if (data$Close[i] > data$Close[i + 1]) {
-      data$B[i] <- +1
-    } else if (data$Close[i] == data$Close[i + 1]) {
-      data$B[i] <- 0
-    } else if (data$Close[i] > data$Low[i + 1]) {
-      data$B[i] <- -1
-    } else data$B[i] <- -2
-  }
-  data$B[nrow(data)] <- NA
-
   # The relative close of the bar inside the bar
-  data$C <- (data$Close - data$Low)/(data$High - data$Low) *
-    100
+  data$C <- (data$Close - data$Low) / (data$High - data$Low)
 
-  # The relative volume of bar
-  for (i in 1:(nrow(data) - period)) {
-    data$V[i] <- data$Volume[i]/mean(data$Volume[i:(i + period -
-      1)])
-  }
-  data$V[(nrow(data) - period + 1):nrow(data)] <- NA
+  data$B <- numeric(nrow(data))
+  data$V <- numeric(nrow(data))
+  data$H <- numeric(nrow(data))
+  data$Ce <- numeric(nrow(data))
+  data$Change <- numeric(nrow(data))
+  data$M <- numeric(nrow(data))
 
-  # The relative height of bar
-  for (i in 1:(nrow(data) - period)) {
-    data$H[i] <- data$Height[i]/mean(data$Height[i:(i + period -
-      1)])
+  for (i in 2:nrow(data)) {
+    # Type of bar
+    if (data$Close[i] > data$High[i - 1]) {
+      data$B[i] <- 1.00
+    }
+    else if (data$Close[i] > data$Close[i - 1]) {
+      data$B[i] <- 0.75
+    }
+    else if (data$Close[i] == data$Close[i - 1]) {
+      data$B[i] <- 0.5
+    }
+    else if (data$Close[i] < data$Low[i - 1]) {
+      data$B[i] <- 0.25
+    }
+    else
+      data$B[i] <- 0.00
+
+    # The difference between closes
+    data$Change[i] <- abs(data$Close[i] - data$Close[i - 1])
   }
-  data$H[(nrow(data) - period + 1):nrow(data)] <- NA
+  data$B[1] <- NA
+  data$Change[1] <- mean(data$Change[1:period])
+
+  for (i in period:nrow(data)) {
+    # The relative volume of bar
+    aver.volume <- .getAver(data$Volume, period, i)
+    if (data$Volume[i] >= aver.volume * 2) {
+      data$V[i] = 1.0
+    }
+    else if (data$Volume[i] >= aver.volume * 1.713) {
+      data$V[i] = 0.8
+    }
+    else if (data$Volume[i] >= aver.volume * 1.148) {
+      data$V[i] = 0.6
+    }
+    else if (data$Volume[i] >= aver.volume * 0.825) {
+      data$V[i] = 0.4
+    }
+    else if (data$Volume[i] >= aver.volume * 0.287) {
+      data$V[i] = 0.2
+    }
+    else
+      data$V[i] = 0.0
+
+    # The relative height of bar
+    aver.height <- .getAver(data$Height, period, i)
+    if (data$Height[i] >= aver.height * 2) {
+      data$H[i] = 1.0
+    }
+    else if (data$Height[i] >= aver.height * 1.713) {
+      data$H[i] = 0.8
+    }
+    else if (data$Height[i] >= aver.height * 1.148) {
+      data$H[i] = 0.6
+    }
+    else if (data$Height[i] >= aver.height * 0.825) {
+      data$H[i] = 0.4
+    }
+    else if (data$Height[i] >= aver.height * 0.287) {
+      data$H[i] = 0.2
+    }
+    else
+      data$H[i] = 0.0
+
+    # The relative close of bar
+    n <- i - period + 1
+    data$Ce[i] <- (data$Close[i] - min(data$Low[n:i])) /
+      (max(data$High[n:i]) - min(data$Low[n:i]))
+
+
+    # Relative price change of the bar
+    aver.change <- .getAver(data$Change, period, i)
+    if (data$Change[i] >= aver.change * 2) {
+      data$M[i] = 1.0
+    }
+    else if (data$Change[i] >= aver.change * 1.713) {
+      data$M[i] = 0.8
+    }
+    else if (data$Change[i] >= aver.change * 1.148) {
+      data$M[i] = 0.6
+    }
+    else if (data$Change[i] >= aver.change * 0.825) {
+      data$M[i] = 0.4
+    }
+    else if (data$Change[i] >= aver.change * 0.287) {
+      data$M[i] = 0.2
+    }
+    else
+      data$M[i] = 0.0
+  }
+  data$V[1:(period - 1)] <- NA
+  data$H[1:(period - 1)] <- NA
+  data$Ce[1:(period - 1)] <- NA
+  data$M[1:(period - 1)] <- NA
   data$Height <- NULL
-
-  # The relative close of bar
-  for (i in 1:(nrow(data) - period)) {
-    n <- i + period - 1
-    data$Ce[i] <- (data$Close[i] - min(data$Low[i:n]))/(max(data$High[i:n]) -
-      min(data$Low[i:n]))
-  }
-  data$Ce[(nrow(data) - period + 1):nrow(data)] <- NA
-
-  # The difference between closes
-  for (i in 1:(nrow(data) - 1)) {
-    data$Change[i] <- abs(data$Close[i] - data$Close[i +
-      1])
-  }
-  data$Change[nrow(data)] <- NA
-
-  # Relative price change of the bar
-  data$M <- rep(0, nrow(data))
-  for (i in 1:(nrow(data) - period)) {
-    data$M[i] <- data$Change[i]/mean(data$Change[i:(i + period -
-      1)])
-  }
-  data$M[(nrow(data) - period + 1):nrow(data)] <- NA
   data$Change <- NULL
 
-  if (standardize) {
-    data[, 3:ncol(data)] <- as.data.frame(scale(data[, 3:ncol(data)]))
-  }
+  return(data[period:nrow(data), ])
+}
 
-  return(data[1:(nrow(data) - period), ])
+.getAver <- function(v, period, i) {
+  return(mean(v[(i - period + 1):i]))
 }
