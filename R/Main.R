e15bd55b3549c66959e97e0f18000b9211fdd71a
Arkadiy <arkada38@outlook.com>
2017-03-19 07:50:29 +0700

    getVsa accepts vector of periods

--- R/Main.R
+++ R/Main.R
@@ -1,12 +1,11 @@
 # d <- getHistoricalQuote('AAPL', 'NASD')
-# d.vsa <- getHistoricalVSAQuote(d)
-# d.vsa <- getHistoricalVSAQuote(d, standardize = T)
+# d.vsa <- getVsa(d)
+# d.vsa <- getVsa(d, period = c(8, 15))
 # d <- getHistoricalQuote('GOOGL', 'NASD', interval = 3600, period = '1Y')
 # d <- getHistoricalQuote('gbpusd', 'CURRENCY', interval = 900, period = '3d')
 # head(d, 15)
 
-getHistoricalQuote <- function(symbol, exchange,
-                               interval = 86400, period = "10Y") {
+getHistoricalQuote <- function(symbol, exchange, interval = 86400, period = "10Y") {
   url <- paste0("https://www.google.com/finance/getprices?q=",
     toupper(symbol), "&x=", toupper(exchange), "&i=", interval,
     "&p=", period, "&f=d,c,v,k,o,h,l")
@@ -18,131 +17,137 @@ getHistoricalQuote <- function(symbol, exchange,
     names(df) <- c("UTC_Date", "Timezon_offset", "Close",
       "High", "Low", "Open", "Volume", "CDAYS")
 
-    df$UTC_Date <- as.POSIXct(df$UTC_Date, origin = "1970-01-01",
-      tz = "UTC")
+    df$UTC_Date <- as.POSIXct(df$UTC_Date, origin = "1970-01-01", tz = "UTC")
     df$Market_Date <- df$UTC_Date + df$Timezon_offset * 60
 
-    df <- data.frame(df$UTC_Date, df$Market_Date, df$Open,
-      df$High, df$Low, df$Close, df$Volume)
-    names(df) <- c("UTC_Date", "Market_Date", "Open", "High",
-      "Low", "Close", "Volume")
+    df <- data.frame(
+      df$UTC_Date, df$Market_Date, df$Open, df$High, df$Low, df$Close, df$Volume
+      )
+    names(df) <- c(
+      "UTC_Date", "Market_Date", "Open", "High", "Low", "Close", "Volume"
+      )
 
     return(df)
   } else warning("Incorrect parameters")
 }
 
-getHistoricalVSAQuote <- function(data, period = 15) {
+getVsa <- function(df, period = 15) {
+  vsa <- data.frame(
+    Height = numeric(nrow(df)),
+    C = numeric(nrow(df)),
+    B = numeric(nrow(df)),
+    Change = numeric(nrow(df))
+  )
+
   # Height of bar
-  data$Height <- data$High - data$Low
+  vsa$Height <- df$High - df$Low
 
   # The relative close of the bar inside the bar
-  data$C <- (data$Close - data$Low) / (data$High - data$Low)
+  vsa$C <- (df$Close - df$Low) / (df$High - df$Low)
 
-  data$B <- numeric(nrow(data))
-  data$V <- numeric(nrow(data))
-  data$H <- numeric(nrow(data))
-  data$Ce <- numeric(nrow(data))
-  data$Change <- numeric(nrow(data))
-  data$M <- numeric(nrow(data))
-
-  for (i in 2:nrow(data)) {
+  for (i in 2:nrow(df)) {
     # Type of bar
-    if (data$Low[i] > data$High[i - 1])
-      data$B[i] <- 6/6
-    else if (data$Close[i] > data$High[i - 1])
-      data$B[i] <- 5/6
-    else if (data$Close[i] > data$Close[i - 1])
-      data$B[i] <- 4/6
-    else if (data$Close[i] == data$Close[i - 1])
-      data$B[i] <- 3/6
-    else if (data$High[i] < data$Low[i - 1])
-      data$B[i] <- 0/6
-    else if (data$Close[i] < data$Low[i - 1])
-      data$B[i] <- 1/6
-    else if (data$Close[i] < data$Close[i - 1])
-      data$B[i] <- 2/6
+    if (df$Low[i] > df$High[i - 1])
+      vsa$B[i] <- 6/6
+    else if (df$Close[i] > df$High[i - 1])
+      vsa$B[i] <- 5/6
+    else if (df$Close[i] > df$Close[i - 1])
+      vsa$B[i] <- 4/6
+    else if (df$Close[i] == df$Close[i - 1])
+      vsa$B[i] <- 3/6
+    else if (df$High[i] < df$Low[i - 1])
+      vsa$B[i] <- 0/6
+    else if (df$Close[i] < df$Low[i - 1])
+      vsa$B[i] <- 1/6
+    else if (df$Close[i] < df$Close[i - 1])
+      vsa$B[i] <- 2/6
 
     # The difference between closes
-    data$Change[i] <- abs(data$Close[i] - data$Close[i - 1])
+    vsa$Change[i] <- abs(df$Close[i] - df$Close[i - 1])
   }
-  data$B[1] <- NA
-  data$Change[1] <- mean(data$Change[1:period])
+  vsa$B[1] <- NA
+  vsa$Change[1] <- mean(vsa$Change[1:3])
 
-  for (i in period:nrow(data)) {
+  for (i in 1:length(period)) {
+    vsa <- data.frame(vsa, .getVsaPeriod(df, vsa, period[i]))
+  }
+
+  vsa$Height <- NULL
+  vsa$Change <- NULL
+
+  return(vsa)
+}
+
+.getVsaPeriod <- function(df, vsa, period) {
+  vsa.period <- data.frame(
+    V = numeric(nrow(df)),
+    H = numeric(nrow(df)),
+    Ce = numeric(nrow(df)),
+    M = numeric(nrow(df))
+  )
+
+  for (i in period:nrow(df)) {
     # The relative volume of bar
-    aver.volume <- .getAver(data$Volume, period, i)
-    if (data$Volume[i] >= aver.volume * 2) {
-      data$V[i] = 1.0
-    }
-    else if (data$Volume[i] >= aver.volume * 1.713) {
-      data$V[i] = 0.8
-    }
-    else if (data$Volume[i] >= aver.volume * 1.148) {
-      data$V[i] = 0.6
-    }
-    else if (data$Volume[i] >= aver.volume * 0.825) {
-      data$V[i] = 0.4
-    }
-    else if (data$Volume[i] >= aver.volume * 0.287) {
-      data$V[i] = 0.2
-    }
+    aver.volume <- .getAver(df$Volume, period, i)
+    if (df$Volume[i] >= aver.volume * 2)
+      vsa.period$V[i] = 1.0
+    else if (df$Volume[i] >= aver.volume * 1.713)
+      vsa.period$V[i] = 0.8
+    else if (df$Volume[i] >= aver.volume * 1.148)
+      vsa.period$V[i] = 0.6
+    else if (df$Volume[i] >= aver.volume * 0.825)
+      vsa.period$V[i] = 0.4
+    else if (df$Volume[i] >= aver.volume * 0.287)
+      vsa.period$V[i] = 0.2
     else
-      data$V[i] = 0.0
+      vsa.period$V[i] = 0.0
 
     # The relative height of bar
-    aver.height <- .getAver(data$Height, period, i)
-    if (data$Height[i] >= aver.height * 2) {
-      data$H[i] = 1.0
-    }
-    else if (data$Height[i] >= aver.height * 1.713) {
-      data$H[i] = 0.8
-    }
-    else if (data$Height[i] >= aver.height * 1.148) {
-      data$H[i] = 0.6
-    }
-    else if (data$Height[i] >= aver.height * 0.825) {
-      data$H[i] = 0.4
-    }
-    else if (data$Height[i] >= aver.height * 0.287) {
-      data$H[i] = 0.2
-    }
+    aver.height <- .getAver(vsa$Height, period, i)
+    if (vsa$Height[i] >= aver.height * 2)
+      vsa.period$H[i] = 1.0
+    else if (vsa$Height[i] >= aver.height * 1.713)
+      vsa.period$H[i] = 0.8
+    else if (vsa$Height[i] >= aver.height * 1.148)
+      vsa.period$H[i] = 0.6
+    else if (vsa$Height[i] >= aver.height * 0.825)
+      vsa.period$H[i] = 0.4
+    else if (vsa$Height[i] >= aver.height * 0.287)
+      vsa.period$H[i] = 0.2
     else
-      data$H[i] = 0.0
+      vsa.period$H[i] = 0.0
 
     # The relative close of bar
     n <- i - period + 1
-    data$Ce[i] <- (data$Close[i] - min(data$Low[n:i])) /
-      (max(data$High[n:i]) - min(data$Low[n:i]))
+    vsa.period$Ce[i] <- (df$Close[i] - min(df$Low[n:i])) /
+      (max(df$High[n:i]) - min(df$Low[n:i]))
 
 
     # Relative price change of the bar
-    aver.change <- .getAver(data$Change, period, i)
-    if (data$Change[i] >= aver.change * 2) {
-      data$M[i] = 1.0
-    }
-    else if (data$Change[i] >= aver.change * 1.713) {
-      data$M[i] = 0.8
-    }
-    else if (data$Change[i] >= aver.change * 1.148) {
-      data$M[i] = 0.6
-    }
-    else if (data$Change[i] >= aver.change * 0.825) {
-      data$M[i] = 0.4
-    }
-    else if (data$Change[i] >= aver.change * 0.287) {
-      data$M[i] = 0.2
-    }
+    aver.change <- .getAver(vsa$Change, period, i)
+    if (vsa$Change[i] >= aver.change * 2)
+      vsa.period$M[i] = 1.0
+    else if (vsa$Change[i] >= aver.change * 1.713)
+      vsa.period$M[i] = 0.8
+    else if (vsa$Change[i] >= aver.change * 1.148)
+      vsa.period$M[i] = 0.6
+    else if (vsa$Change[i] >= aver.change * 0.825)
+      vsa.period$M[i] = 0.4
+    else if (vsa$Change[i] >= aver.change * 0.287)
+      vsa.period$M[i] = 0.2
     else
-      data$M[i] = 0.0
+      vsa.period$M[i] = 0.0
   }
-  data$V[1:(period - 1)] <- NA
-  data$H[1:(period - 1)] <- NA
-  data$Ce[1:(period - 1)] <- NA
-  data$M[1:(period - 1)] <- NA
-  data$Height <- NULL
-  data$Change <- NULL
+  vsa.period$V[1:(period - 1)] <- NA
+  vsa.period$H[1:(period - 1)] <- NA
+  vsa.period$Ce[1:(period - 1)] <- NA
+  vsa.period$M[1:(period - 1)] <- NA
 
-  return(data[period:nrow(data), ])
+  names(vsa.period) <- c(paste0(
+    "V_", period), paste0("H_", period), paste0("Ce_", period), paste0("M_", period)
+    )
+
+  return(vsa.period)
 }
 
 .getAver <- function(v, period, i) {
